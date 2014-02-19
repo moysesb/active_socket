5c2a4e136ea9863097b1b6ad76264c533c6e8db4
Moyses Borges <moysesb@gmail.com>
2014-02-18 21:24:02 -0300

    Semi implementacao

--- ActiveTCPClient.cpp
+++ ActiveTCPClient.cpp
@@ -24,15 +24,61 @@ static const char* state_names[] = {
     "CLOSED",
 };
 
-ActiveTCPClient::ActiveTCPClient() {
-    state = UNKNOWN;
-    sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+ActiveTCPClient::ActiveTCPClient() : 
+        state(UNKNOWN), 
+        sockfd(0),
+        remote_address("localhost"), 
+        remote_port(DEFAULT_SERVER_PORT),
+        last_error(NO_ERROR) {
 }
 
-ActiveTCPClient::ActiveTCPClient(const string& remote_ip, const uint16_t remote_port) {
+ActiveTCPClient::ActiveTCPClient(const string& remote_ip, 
+                                        const uint16_t remote_port) :
+        state(CONFIGURED), 
+        sockfd(0),
+        remote_address(remote_ip),
+        remote_port(remote_port),
+        last_error(NO_ERROR) {
     
 }
 
+bool ActiveTCPClient::connect(const string& ip, const uint16_t port) {
+    state = CONFIGURED;
+    remote_address.assign(ip);
+    remote_port = port;
+    return this->connect();
+}
+
+bool ActiveTCPClient::connect() {
+    if(state != CONFIGURED ||
+            remote_address.empty() || remote_port == 0) {
+        return false;
+    }
+    
+    int sockfd, portno, n;
+
+    char buffer[256];
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    
+    struct addrinfo hint_info = {0};
+    hint_info.ai_family = AF_INET;
+    hint_info.ai_protocol = IPPROTO_TCP;
+    hint_info.ai_flags = AI_NUMERICHOST | AI_NUMERICSERV;
+    
+    struct addrinfo* srv_info; 
+    n = getaddrinfo("192.168.1.16", "7000", &hint_info, &srv_info);
+    
+    if (srv_info->ai_addr == NULL) {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+    if (connect(sockfd, srv_info->ai_addr, srv_info->ai_addrlen) < 0)
+        error("ERROR connecting");
+    
+}
 ActiveTCPClient::ActiveTCPClient(const ActiveTCPClient& orig) {
 }
 
--- ActiveTCPClient.h
+++ ActiveTCPClient.h
@@ -16,6 +16,16 @@
 
 using namespace std;
 
+#define DEFAULT_SERVER_PORT 7000
+
+enum SOCKET_ERROR {
+    NO_ERROR,
+    NOT_CONFIGURED,
+    BUSY,
+    IO_ERROR,
+    CONNECTION_REFUSED,
+    MEMORY_ERROR,
+};
 enum SOCKET_STATE {
     UNKNOWN,
     CONFIGURED,
@@ -28,26 +38,31 @@ class ActiveTCPClient {
 public:
     ActiveTCPClient();
     ActiveTCPClient(const string& remote_ip, const uint16_t remote_port);
-    ActiveTCPClient(const ActiveTCPClient& orig);
     virtual ~ActiveTCPClient();
 
     virtual bool connect();
     virtual bool connect(const string& ip, const uint16_t port);
+    virtual void reset();
     
-    virtual string      next_line(size_t maxlen = 255);
-    virtual char        next_char();
-    virtual string      next_chunk(size_t length);
-    virtual void        close();
+    
+    virtual string next_line(size_t maxlen = 255);
+    virtual char   next_char();
+    virtual string next_chunk(size_t length);
+    virtual void   close();
     
     void start();
     void stop();
 private:
-    SOCKET_STATE state;
-    int sockfd;
-    string remote_address;
-    short  remote_port;
+    ActiveTCPClient(const ActiveTCPClient& orig);
+    
+    SOCKET_STATE        state;
+    int                 sockfd;
+    string              remote_address;
+    short               remote_port;
     
     string partial_line;
+    
+    SOCKET_ERROR last_error;
 };
 
 #endif	/* ACTIVETCPCLIENT_H */
